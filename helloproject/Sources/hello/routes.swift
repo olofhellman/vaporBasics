bb5b83fa913ef190909e2a4c8548ac1247942411
Olof Hellman <Olof.Hellman@ametek.com>
2026-03-11 21:32:31 +0000

    add error handling

--- helloproject/Sources/hello/routes.swift
+++ helloproject/Sources/hello/routes.swift
@@ -8,14 +8,28 @@ func routes(_ app: Application) throws {
 
     app.get(":token") { req async throws -> Response in
         
+        func textResponse(_ text: String, status: HTTPResponseStatus = .ok) -> Response {
+            let response = Response(status: status)
+            response.headers.replaceOrAdd(name: .contentType, value: "text/plain; charset=utf-8")
+            response.body = .init(string: text)
+            return response
+        }
+
         guard let shortCode = req.parameters.get("token") else {
-            throw Abort(.notFound)
+            return textResponse("For whatever reason, the token was missing from the URL. Sorry", status: .notFound)
         }
  
         guard let link = try? await Link.query(on: req.db)
             .filter(\.$shortCode == shortCode)
-            .first() else { throw Abort(.notFound) }
+            .first() else { return textResponse("There's no link associated with that token", status: .notFound) }
         
+          // handle the case where the url had a ?debug=true query parameter
+        if let debug = try? req.query.get(Bool.self, at: "debug"), debug {
+            let shortCodeString = "token: " + shortCode
+            let redirection = "redirects to: " + link.url  
+            return textResponse(shortCodeString + "\n" + redirection)
+        }
+
         return req.redirect(to: link.url)
     }
 
