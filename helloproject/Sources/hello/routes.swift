e904267209ffcdd2a8274f737f1efa5a03b665c0
Olof Hellman <Olof.Hellman@ametek.com>
2026-03-11 20:09:37 +0000

    provide redirect code

--- helloproject/Sources/hello/routes.swift
+++ helloproject/Sources/hello/routes.swift
@@ -1,12 +1,22 @@
 import Vapor
+import Fluent
 
 func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get(":token") { req async throws -> Response in
+        
+        guard let shortCode = req.parameters.get("token") else {
+            throw Abort(.notFound)
+        }
+ 
+        guard let link = try? await Link.query(on: req.db)
+            .filter(\.$shortCode == shortCode)
+            .first() else { throw Abort(.notFound) }
+        
+        return req.redirect(to: link.url)
     }
 
     try app.grouped("api").register(collection: LinksController())
